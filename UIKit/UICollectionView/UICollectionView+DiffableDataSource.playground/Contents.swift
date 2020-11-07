6b357fd97c1e101680e2885066482a40cb2b2699
Ryan Thally <ryan.thally@gmail.com>
2020-11-07 10:29:27 -0500

    Updated documentation

--- UIKit/UICollectionView/UICollectionView+DiffableDataSource.playground/Contents.swift
+++ UIKit/UICollectionView/UICollectionView+DiffableDataSource.playground/Contents.swift
@@ -4,16 +4,17 @@ import UIKit
 import PlaygroundSupport
 
 class MyViewController : UIViewController {
-    // Hold a reference to the data source to apply updates later (Collection view has a weak reference.)
+    // 1. Hold a reference to the data source to apply updates later (Collection view has a weak reference.)
     private var diffableDataSource: UICollectionViewDiffableDataSource<Int, Int>!
     
+    // 2. Configure the Collection View and assign to the VC
     override func loadView() {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
         collectionView.delegate = self
         collectionView.register(TextCell.self, forCellWithReuseIdentifier: TextCell.reuseIdentifer)
         
         /*
-        1.  Implement UICollectionViewDataSource. Takes Section and Item Identifiers as generic parameters. Initializer takes a
+        2a.  Implement UICollectionViewDataSource. Takes Section and Item Identifiers as generic parameters. Initializer takes a
             UICollectionView and a cell provider closure (equalivelent to "cellForRowAt:indexPath:")
             Assign to a seperate variable instead of directly to collection view, since UICollectionView.dataSource has
             a **weak** referece and
@@ -26,18 +27,18 @@ class MyViewController : UIViewController {
             return cell
         }
         
-        // Assign data source to collection view.
+        // 2b. Assign data source to collection view.
         collectionView.dataSource = diffableDataSource
         
         self.view = collectionView
         self.view.backgroundColor = .white
     }
     
-    // Load the data once the view is configured.
+    // 3. Load the data once the view is configured.
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load some data to a NSDiffableDataSourceSnapshot
+        // 3a. Load some data to a NSDiffableDataSourceSnapshot
         var snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
         snapshot.appendSections([0])
         snapshot.appendItems(Array(0..<10))
@@ -46,20 +47,20 @@ class MyViewController : UIViewController {
     }
 }
 
-// 3. Conform to the UICollectionViewDelegate protocol - specifies how the collection will respond to events
+// 4. Conform to the UICollectionViewDelegate protocol - specifies how the collection will respond to events
 extension MyViewController: UICollectionViewDelegate {
-    // 3a. Provides a response to an item tap (Optional)
+    // 4a. Provides a response to an item tap (Optional)
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("Tapped: Section(\(indexPath.section)), Row(\(indexPath.row))")
     }
 }
 
-// 4. Provide a cell to display in the collection view.
+// 5. Provide a cell to display in the collection view.
 class TextCell: UICollectionViewCell {
-    // 4a. Providing a static reuse identifier unique to the cell simplifies configuration
+    // 5a. Providing a static reuse identifier unique to the cell simplifies configuration
     static let reuseIdentifer: String = "TextCell"
     
-    // 4b. Create a simple label to display some text.
+    // 5b. Create a simple label to display some text.
     //     Closure instantiation contains the label cofiguration to block for better orgainization
     var titleLabel: UILabel = {
         let label = UILabel()
@@ -67,7 +68,7 @@ class TextCell: UICollectionViewCell {
         return label
     }()
     
-    // 4c. Override the instantiation to configure view hiearchy and autolayout.
+    // 5c. Override the instantiation to configure view hiearchy and autolayout.
     override init(frame: CGRect) {
         // Don't forget to initialize the superclass
         super.init(frame: frame)
@@ -87,7 +88,7 @@ class TextCell: UICollectionViewCell {
         self.backgroundColor = .red
     }
     
-    // 4d. Required by the compiler to use with Storyboards.
+    // 5d. Required by the compiler to use with Storyboards.
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
