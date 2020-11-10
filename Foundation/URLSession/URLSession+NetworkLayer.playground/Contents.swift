d117792416a2728ebeccc5bb532a9eeb06bd978d
Ryan Thally <ryan.thally@gmail.com>
2020-11-10 12:24:16 -0500

    Updated documentation

--- Foundation/URLSession/URLSession+NetworkLayer.playground/Contents.swift
+++ Foundation/URLSession/URLSession+NetworkLayer.playground/Contents.swift
@@ -5,6 +5,7 @@ NetworkEngine.request(endpoint: JSONPlaceholderAPIEndpoint.getPosts) { (result: 
     switch result {
     case .success(let posts):
         // 1a. Do something with the valid posts.
+        print("--- All Posts ---")
         print(posts.count)
         print(posts[0])
         
@@ -14,3 +15,18 @@ NetworkEngine.request(endpoint: JSONPlaceholderAPIEndpoint.getPosts) { (result: 
     }
 }
 
+// 2. Fetch a specifc post using the network engine.
+NetworkEngine.request(endpoint: JSONPlaceholderAPIEndpoint.getPost(id: "1")) { (result: Result<JSONPlaceholderPost, Error>) in
+    switch result {
+    case .success(let post):
+        // 1a. Do something with the valid posts.
+        print("--- One Post ---")
+        print(post)
+        
+    case .failure(let error):
+        // 1b. Do something else with the error.
+        print(error.localizedDescription)
+    }
+}
+
+// Notice how the print statements may or may not be in the same order as the request above. Due to the asynchronous nature of URLSession, it will print based on the order of completion.
--- Foundation/URLSession/URLSession+NetworkLayer.playground/Sources/NetworkEngine.swift
+++ Foundation/URLSession/URLSession+NetworkLayer.playground/Sources/NetworkEngine.swift
@@ -3,6 +3,7 @@ import Foundation
 /// Provides network communitcation abilities via URLSession
 public class NetworkEngine {
     public class func request<T: Codable>(endpoint: Endpoint, completion: @escaping (Result<T, Error>) -> ()) {
+        // Create a URL from the endpoint components.
         var components = URLComponents()
         components.scheme = endpoint.scheme
         components.host = endpoint.host
@@ -11,18 +12,23 @@ public class NetworkEngine {
         
         guard let url = components.url else { return }
         
+        // Create a requst for the desired url with the httpMethod specified by the Endpoint
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = endpoint.method
         
-        let session = URLSession(configuration: .default)
+        // Create a new dataTask to fetch the url request
+        let session = URLSession.shared
         let dataTask = session.dataTask(with: urlRequest) { data, response, error in
+            // Handle any errors with the fetch
             guard error == nil else {
                 completion(.failure(error!))
                 return
             }
             
+            // Confirm the response and data are available
             guard response != nil, let data = data else { return }
             
+            // Process the data on the main thread
             DispatchQueue.main.async {
                 if let responseObject = try? JSONDecoder().decode(T.self, from: data) {
                     completion(.success(responseObject))
@@ -34,6 +40,7 @@ public class NetworkEngine {
             }
         }
         
+        // Run the data task.
         dataTask.resume()
     }
 }
